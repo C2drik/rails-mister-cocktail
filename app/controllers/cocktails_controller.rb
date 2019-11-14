2507e21bfc09854325b90380c1e6f48e46fd19b5
Cedrik Razafimanantsoa <cedrik.razafi@gmail.com>
2019-11-14 14:21:58 +0100

    Models done

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,20 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+  end
+
+
+
+private
+
+
+end
